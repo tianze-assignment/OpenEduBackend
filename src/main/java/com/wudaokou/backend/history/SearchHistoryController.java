cfcd8964ee6b6733163cabeb1757e787c9a119b1
tz039e <tz039e@gmail.com>
2021-08-21 15:37:12 +0800

    fix validation of POST search history

--- src/main/java/com/wudaokou/backend/history/SearchHistoryController.java
+++ src/main/java/com/wudaokou/backend/history/SearchHistoryController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class SearchHistoryController {
     private final SearchHistoryRepository searchHistoryRepository;
@@ -17,7 +19,7 @@ public class SearchHistoryController {
     }
 
     @PostMapping ("/api/history/search")
-    ResponseEntity<?> test(@RequestBody SearchHistory searchHistory){
+    ResponseEntity<?> post(@Valid @RequestBody SearchHistory searchHistory){
         searchHistory.setCustomer(securityRelated.getCustomer());
         searchHistoryRepository.save(searchHistory);
         return ResponseEntity.ok("ok");
